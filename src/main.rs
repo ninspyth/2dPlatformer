3e1798f3358fa7df1e00796245a7f906f32abb5b
ninspyth <amandavu11@gmail.com>
2023-09-15 16:11:22 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+mod world;
+
+use bevy::prelude::*;
+use::bevy_ecs_ldtk::prelude::*;
+use::bevy_asset_loader::prelude::*;
+use world::*;
+
+fn main() {
+    App::new()
+        .insert_resource(LevelSelection::Index(0))
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Knight Rush".to_string(),
+                    resolution: (1280.0, 720.0).into(),
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(ImagePlugin::default_nearest())
+        )
+        .add_plugins(LdtkPlugin)
+        .add_systems(Startup, (load_background, load_map))
+        .run()
+}
+
---
+++ src/world.rs
@@ -0,0 +1,65 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+use bevy_asset_loader::prelude::*;
+
+/*#[derive(AssetCollection, Resource)]
+pub struct ImageAssets {
+    #[asset(path = "map.ldtk")]
+    map: Handle<LdtkAsset>,
+}*/
+
+
+pub fn load_background(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>
+) {
+    //spawn a camera
+    commands.spawn(Camera2dBundle::default());
+
+    //loading the background
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(1280.0, 720.0)),
+            ..default()
+        },
+        texture: asset_server.load("background/background1.png"),
+        ..default()
+    });
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(1280.0, 720.0)),
+            ..default()
+        },
+        texture: asset_server.load("background/background2.png"),
+        ..default()
+    });
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(1280.0, 720.0)),
+            ..default()
+        },
+        texture: asset_server.load("background/background3.png"),
+        ..default()
+    });
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(1280.0, 720.0)),
+            ..default()
+        },
+        texture: asset_server.load("background/background4.png"),
+        ..default()
+    });
+}
+
+
+pub fn load_map(
+    mut commands: Commands,
+    images: Res<AssetServer>
+) {
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: images.load("map1.ldtk"),
+        transform: Transform::from_xyz(-640.0, -360.0, 0.0),
+        ..default()
+    });
+}
